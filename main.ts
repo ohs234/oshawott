b934680fc81c9c769ca3c8c12ec8890d54f5e7a1
ohs234 <68495056+ohs234@users.noreply.github.com>
2020-07-19 11:35:55 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+namespace SpriteKind {
+    export const Slugsy = SpriteKind.create()
+    export const Ksjsisi = SpriteKind.create()
+    export const Jskskzkzkz = SpriteKind.create()
+    export const Meme = SpriteKind.create()
+    export const Momo = SpriteKind.create()
+}
+let Suggsy = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 6 . . . . 6 . . . . . . . 
+. . . . 8 8 8 8 . . . . . . . . 
+. . . 8 9 4 4 9 8 . . . 9 . . . 
+. 9 . 8 4 4 4 4 8 . . 9 . . . . 
+9 . . 9 4 4 4 4 9 9 9 . . . . . 
+9 9 9 9 4 4 4 4 4 9 9 . . . . . 
+9 9 9 9 9 9 9 9 9 9 9 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Slugsy)
+let Mzmz = sprites.create(img`
+. . . . . . . . . . . 7 7 7 7 7 
+. . . . . . . . . . . 7 7 7 7 7 
+. . . . . . . . . 7 7 7 7 7 7 7 
+. . . . . . . 7 7 7 5 7 7 7 7 7 
+. . . . . 7 7 5 5 5 5 7 7 7 7 7 
+. . . . 7 7 5 5 5 5 5 7 7 7 7 . 
+. . . 7 7 5 5 5 5 5 7 7 7 7 7 . 
+. . 7 7 5 7 7 5 7 7 7 7 7 7 . . 
+. 7 7 5 5 5 5 5 5 7 7 7 7 7 . . 
+. 7 5 5 5 5 5 5 5 7 7 7 7 . . . 
+. 7 7 5 5 5 5 5 7 7 7 7 . . . . 
+. 7 7 5 5 5 7 7 7 7 7 7 . . . . 
+. 7 7 5 5 7 7 7 7 7 7 . . . . . 
+. 7 7 7 7 7 7 7 7 7 7 7 . . . . 
+7 7 7 7 7 7 7 7 7 . . . 7 7 . . 
+7 . . 7 7 7 . . . . . . . 7 7 . 
+`, SpriteKind.Meme)
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. d d d d d d d d d d d d . . . 
+. d d d d d d d d d d d d . . . 
+. d f f f f d d f f f f d . . . 
+. d f 1 1 f d d f 1 1 f d . . . 
+. d f 1 1 f d d f 1 1 f d . . . 
+. d f f f f d d f f f f d . . . 
+d d d d d d d d d d d d d d . . 
+. d d d d d d d d d d d d . . . 
+d d d d d d d d d d d d d d . . 
+d d d d d d d d d d d d d d . . 
+d d d d d d d d d d d d d d . . 
+d d d d d d d d d d d d d d . . 
+d . . . . . . . . . . . . d . . 
+d d d . . . . . . . . . . d d d 
+`, SpriteKind.Jskskzkzkz)
+controller.moveSprite(Suggsy)
